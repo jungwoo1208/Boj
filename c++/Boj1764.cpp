5db59b2386ead88c149866f7a7a16dd54988c8ab
jungwoo <gjw1972@naver.com>
2023-05-24 13:59:56 +0900

    setting

--- c++/Boj1764.cpp
+++ c++/Boj1764.cpp
@@ -4,26 +4,30 @@
 #include <set>
 using namespace std;
 int main(){
-    int a,b;
-    cin>> a >> b;
-    string arr1 [a];
-    string arr2 [b];
-    for(int i=0;i<a;i++){
-        string k;
-        cin>>k;
-        arr1[i]=k;
+    int a, b;
+    cin >> a >> b;
+    set<string> set1;
+    set<string> set2;
+    for(int i=0; i<a; i++){
+        string s;
+        cin >> s;
+        set1.insert(s);
     }
-    for(int i=0;i<b;i++){
-        string k;
-        cin>>k;
-        arr2[i]=k;
+    for(int i=0; i<b; i++){
+        string s;
+        cin >> s;
+        set2.insert(s);
     }
-    set<string> set1(arr1,arr1+a);
-    set<string> set2(arr2,arr2+b);
     set<string> result;
-    set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), inserter(result, result.begin()));
-    cout<<result.size()<<std::endl;
+    set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), std::inserter(result, result.begin()));
+    cout<<result.size()<<endl;
+    string result2[result.size()];
+    int i=0;
     for(string k: result){
-        cout<<k<<std::endl;
+        result2[i++]=k;
+    }
+    sort(result2, result2+result.size());
+    for(string k: result){
+        cout<<k<<endl;
     }
 }
